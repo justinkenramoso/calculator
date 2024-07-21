b67e12cf2a60368e0196815f06b2b54596a5f367
Justin Ken <justinken.ramoso@gmail.com>
2024-07-22 00:07:34 +0800

    Added keyboard support for operations

--- script.js
+++ script.js
@@ -2,14 +2,23 @@ const display = document.querySelector(".display");
 
 const numpad = {
     numbers: ["Numpad0","Numpad1","Numpad2","Numpad3","Numpad4","Numpad5","Numpad6","Numpad7","Numpad8","Numpad9","NumpadDecimal"],
-    operations: ["NumpadAdd","NumpadSubtract","NumpadMultiply","NumpadDivide","NumpadEnter"],
-    backspace: ["Backspace"]
+    operations: ["NumpadAdd","NumpadSubtract","NumpadMultiply","NumpadDivide","NumpadEnter"]
 }
 
 window.addEventListener("keydown", (event) => {
+
+    console.log(event)
+
+    if (event.code === "NumpadEnter" && event.ctrlKey === true) {
+        clear();
+    }
     
     if (numpad.numbers.includes(event.code)) {
         numberPress(event.code.slice(6))
+    } else if (numpad.operations.includes(event.code)) {
+        operate(event);
+    } else if (event.code === "Backspace") {
+        backspace();
     }
 
 })
@@ -21,54 +30,7 @@ numberButtons.forEach(numberBtn => {
 
 const operationButtons = [...document.querySelectorAll(".ops-btn")];
 operationButtons.forEach(operationBtn => {
-    operationBtn.addEventListener("click", (e) => {
-
-        if (e.target.textContent != "=") {
-            pressTracker.shift();
-            pressTracker.push("ops");
-        }
-
-        if (pressTracker[0] === "ops" && pressTracker[1] === "ops") {
-            alert("Error: cannot press operation twice!")
-            clear();
-        }
-
-        if (!firstNumber) {
-            firstNumber = parseInt(display.textContent);
-            operation = e.target.textContent;
-            clearOnNextPress = true;
-        } else if (firstNumber && !secondNumber){
-
-            secondNumber = parseInt(display.textContent);
-
-            if (operation === "÷" && (firstNumber === 0 || secondNumber === 0)) {
-                alert("No. You can't.")
-                clear();
-            }
-
-            switch (operation) {
-                case "+": 
-                    firstNumber = add(firstNumber, secondNumber);
-                    break;
-                case "-": 
-                    firstNumber = subtract(firstNumber, secondNumber);
-                    break;
-                case "x": 
-                    firstNumber = multiply(firstNumber, secondNumber);
-                    break;
-                case "÷": 
-                    firstNumber = divide(firstNumber, secondNumber);
-                    break;
-            }
-            if (firstNumber % 1 != 0) {
-                firstNumber = firstNumber.toFixed(2);
-            }
-            secondNumber = undefined;
-            operation = e.target.textContent;
-            display.textContent = firstNumber;
-            clearOnNextPress = true;
-        }
-    })
+    operationBtn.addEventListener("click", (e) => operate(e))
 })
 
 const clearButton = document.querySelector(".clear-btn");
@@ -116,6 +78,79 @@ function numberPress(number) {
         clearOnNextPress = false;
     }
     display.textContent += number;
+}
+
+function operate(e) {
+
+    let operationPressed = "";
+
+    if (e.type === "click") {
+        operationPressed = e.target.textContent;
+    } else if (e.type === "keydown") {
+        switch(e.code) {
+            case "NumpadAdd":
+                operationPressed = "+"
+                break;
+            case "NumpadSubtract":
+                operationPressed = "-"
+                break;
+            case "NumpadMultiply":
+                operationPressed = "*"
+                break;
+            case "NumpadDivide":
+                operationPressed = "÷"
+                break;
+            case "NumpadEnter":
+                operationPressed = "="
+                break;
+        }
+    }
+
+    if (operationPressed != "=") {
+        pressTracker.shift();
+        pressTracker.push("ops");
+    }
+
+    if (pressTracker[0] === "ops" && pressTracker[1] === "ops") {
+        alert("Error: cannot press operation twice!")
+        clear();
+    }
+
+    if (!firstNumber) {
+        firstNumber = parseInt(display.textContent);
+        operation = operationPressed;
+        clearOnNextPress = true;
+    } else if (firstNumber && !secondNumber){
+
+        secondNumber = parseInt(display.textContent);
+
+        if (operation === "÷" && (firstNumber === 0 || secondNumber === 0)) {
+            alert("No. You can't.")
+            clear();
+        }
+
+        switch (operation) {
+            case "+": 
+                firstNumber = add(firstNumber, secondNumber);
+                break;
+            case "-": 
+                firstNumber = subtract(firstNumber, secondNumber);
+                break;
+            case "x": 
+                firstNumber = multiply(firstNumber, secondNumber);
+                break;
+            case "÷": 
+                firstNumber = divide(firstNumber, secondNumber);
+                break;
+        }
+        if (firstNumber % 1 != 0) {
+            firstNumber = firstNumber.toFixed(2);
+        }
+        secondNumber = undefined;
+        operation = operationPressed;
+        display.textContent = firstNumber;
+        clearOnNextPress = true;
+    }
 }
 
 let firstNumber;
