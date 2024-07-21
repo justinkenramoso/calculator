e7a77ddc630bcf48408e5ce074e6a4b2f03bc79d
Justin Ken <justinken.ramoso@gmail.com>
2024-07-21 22:46:42 +0800

    Added press trackers

--- README.md
+++ README.md
@@ -12,3 +12,25 @@ EEEEEE - OFF WHITE
 B50800 - DARK RED
 0053B5 - BLUE
 00B562 - GREEN
+
+### Calculator Pseudocode
+
+firstNumber;
+secondNumber;
+operation;
+
+user inputs number
+
+operation press: 
+    firstnumber = input; 
+    operation = button pressed;
+    clearOnNextPress = true;
+
+user inputs another number:
+    clearOnNextPress = false;
+    display pressed number;
+
+operation pressed:
+    firstNumber = operate num1 and num2;
+    secondNumber = undefined;
+    operation = button pressed;
--- script.js
+++ script.js
@@ -9,18 +9,51 @@ const operationButtons = [...document.querySelectorAll(".ops-btn")];
 operationButtons.forEach(operationBtn => {
     operationBtn.addEventListener("click", (e) => {
 
-        if (!firstNumber && !secondNumber) {
-            firstNumber = display.textContent;
-            operation = e.target.textContent;
-        } else if (firstNumber && operation) {
-            secondNumber = display.textContent;
-            const answer = operate(firstNumber, secondNumber, operation);
-            display.textContent = answer;
-            firstNumber = answer;
-            operation = e.target.textContent;
-            secondNumber = "";
+        if (e.target.textContent != "=") {
+            pressTracker.shift();
+            pressTracker.push("ops");
         }
 
+        if (pressTracker[0] === "ops" && pressTracker[1] === "ops") {
+            alert("Error: cannot press operation twice!")
+            clear();
+        }
+
+        if (!firstNumber) {
+            firstNumber = parseInt(display.textContent);
+            operation = e.target.textContent;
+            clearOnNextPress = true;
+        } else if (firstNumber && !secondNumber){
+
+            secondNumber = parseInt(display.textContent);
+
+            if (operation === "÷" && (firstNumber === 0 || secondNumber === 0)) {
+                alert("No. You can't.")
+                clear();
+            }
+
+            switch (operation) {
+                case "+": 
+                    firstNumber = add(firstNumber, secondNumber);
+                    break;
+                case "-": 
+                    firstNumber = subtract(firstNumber, secondNumber);
+                    break;
+                case "x": 
+                    firstNumber = multiply(firstNumber, secondNumber);
+                    break;
+                case "÷": 
+                    firstNumber = divide(firstNumber, secondNumber);
+                    break;
+            }
+            if (firstNumber % 1 != 0) {
+                firstNumber = firstNumber.toFixed(2);
+            }
+            secondNumber = undefined;
+            operation = e.target.textContent;
+            display.textContent = firstNumber;
+            clearOnNextPress = true;
+        }
     })
 })
 
@@ -32,46 +65,27 @@ function clear() {
     operation = "";
     secondNumber = undefined;
     display.textContent = "";
+    clearOnNextPress = false;
+    pressTracker = ["",""]
 }
 
 function numberPress(number) {
+
+    pressTracker.shift();
+    pressTracker.push("num");
+
+    if (clearOnNextPress) {
+        display.textContent = "";
+        clearOnNextPress = false;
+    }
     display.textContent += number;
 }
 
 let firstNumber;
 let operation = "";
 let secondNumber;
-
-function operate(num1, num2, ops) {
-
-    let result = 0;
-
-    switch (ops) {
-
-        case "+": 
-        result = num1 + num2;
-        break;
-
-        case "-": 
-        result = num1 - num2;
-        break;
-
-        case "X": 
-        result = num1 * num2;
-        break;
-
-        case "/":
-        result = num1 / num2;
-        break;
-
-        case "=": 
-        
-        break;
-    }
-
-    return result;
-
-}
+let clearOnNextPress = false;
+let pressTracker = ["",""];
 
 function add(num1, num2) {
     return num1 + num2;
